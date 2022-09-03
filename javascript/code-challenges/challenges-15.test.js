e3af3b5646666b6569996804dcbdb53311184799
Dawesome101 <dawesome@danawesome.com>
2022-09-03 15:36:36 -0700

    Completed challenge 15

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -11,8 +11,10 @@ Write a function named screenForNames that takes in an array of strings and uses
 ------------------------------------------------------------------------------------------------ */
 
 const screenForNames = (arr) => {
-  // Solution code here...
-}
+  let regex = /^(Mr\.|Mrs\.|Ms\.|Dr\.)\s[a-zA-Z]+$/;
+
+  return arr.filter(str => regex.test(str));
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -23,7 +25,7 @@ For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyv
 ------------------------------------------------------------------------------------------------ */
 
 const toTitleCase = (arr) => {
-  // Solution code here...
+  return arr.map(str => str.charAt(0).toUpperCase() + str.substring(1));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -98,7 +100,11 @@ let starWarsData = [{
 }];
 
 let biggerThanLuke = (arr) => {
-  // Solution code here...
+  let luke = arr.find(char => char.name === 'Luke Skywalker');
+
+  return arr.filter(personObj => +personObj.mass > +luke.mass)
+    .map(person => person.name)
+    .join(' - ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -116,11 +122,11 @@ This data could be sorted by name or price.
 ------------------------------------------------------------------------------------------------ */
 
 const sortBy = (property, arr) => {
-  // Solution code here...
+  return arr.sort((a, b) => a[property] > b[property] ? 1 : -1 );
 };
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 5 
+CHALLENGE 5
 
 Write a function that determines if a given URL is secure, beginning with https://
 
@@ -132,11 +138,11 @@ https://secure.com returns true because the URL is secure
 https:/missingslash.org returns false because the URL is malformed
 ------------------------------------------------------------------------------------------------ */
 const isSecure = (url) => {
-  // Solution code here...
+  return /(https:\/\/)/.test(url);
 };
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 6 
+CHALLENGE 6
 
 Write a function named detectTicTacToeWin that accepts a two-dimensional array of strings. Each string is guaranteed to be either "X", "O" or an empty string. Your function should check to see if any row, column, or either diagonal direction has three matching "X" or "O" symbols (non-empty strings), three-in-a-line.
 
@@ -155,7 +161,24 @@ Here is a sample board:
 ------------------------------------------------------------------------------------------------ */
 
 const detectTicTacToeWin = (board) => {
-  // Solution code here...
+  const helpCheck = (row1, col1, row2, col2, row3, col3) => {
+    return board[row1][col1] !== '' &&
+    board[row1][col1] === board[row2][col2] &&
+    board[row2][col2] === board[row3][col3];
+  };
+
+  if(helpCheck(0, 0, 0, 1, 0, 2)) return true;
+  if(helpCheck(1, 0, 1, 1, 1, 2)) return true;
+  if(helpCheck(2, 0, 2, 1, 2, 2)) return true;
+
+  if(helpCheck(0, 0, 1, 0, 2, 0)) return true;
+  if(helpCheck(0, 1, 1, 1, 2, 1)) return true;
+  if(helpCheck(0, 2, 1, 2, 2, 2)) return true;
+
+  if(helpCheck(0, 0, 1, 1, 2, 2)) return true;
+  if(helpCheck(0, 2, 1, 1, 2, 0)) return true;
+
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
